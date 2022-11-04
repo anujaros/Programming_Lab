f0f7a3e2f85d0c24337b800c55383a53d9a6b7d8
anujaros <71624832+anujaros@users.noreply.github.com>
2022-11-04 09:16:05 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,9 +1,6 @@
-#list comprehension
-a = input("enter the numbers")
-a = a.split()
-#a = list(map(int,a))
-
 # list positive of positive integers
+a = input("enter the numbers")
+a = a.split()   #a = list(map(int,a))
 pl=list()
 a = [ int(x) for x in a]
 for i in a:
@@ -15,6 +12,17 @@ print("positive integers:",pl)
 #square of N numbers
 b=[x*x for x in a]
 print ("The square of given numbers are:",b)
+
+#list of vowels
+v = input("enter a letter")
+vo = [ "a", "e", "i","o", "u", "A", "E","I","O","U"]
+a =[ x for x in v if x in vo]
+print(a)
+
+#list of ordinal value
+ch = input("enter a letter")
+ch = [ ord(x) for x in ch]
+print(ch)
 
 
 

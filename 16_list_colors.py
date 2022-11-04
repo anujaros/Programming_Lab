a5c49394ff060a397505be6e7da9981d0b70c1a6
anujaros <71624832+anujaros@users.noreply.github.com>
2022-11-04 10:57:17 +0530

    Add files via upload

---
+++ 16_list_colors.py
@@ -0,0 +1,4 @@
+c = input("Enter colours separated by comma:")
+c=c.split(",")
+print(c[0])
+print( c[-1])
---
+++ 20_list_even.py
@@ -0,0 +1,8 @@
+li = input("Enter any integers:")
+li = li.split()
+li = [int(x) for x in li]
+new_l= []
+for i in li:
+    if i%2 != 0:
+        new_l.append(i)
+print("list with odd no:",new_l)        

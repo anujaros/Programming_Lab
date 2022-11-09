f59eceafa8d7d8f5e4ee274705b435ba1c35c524
anujaros <71624832+anujaros@users.noreply.github.com>
2022-11-09 23:25:04 +0530

    Add files via upload

---
+++ 6_leap_year.py
@@ -0,0 +1,13 @@
+#future leap year from current year to a final year
+r= list()
+yr1 = int(input("enter a year:"))
+yr2= int(input("enter any year in the future:"))
+for i in range(yr1,yr2+1):
+	if i%4==0 and i%100== 0 and i%400==0:
+	    r.append(i)
+	elif i%4 ==0  and i%100 !=0:
+	 r.append(i)
+	else:
+	    continue
+
+print ("The leap years are:",r) #list of leap year

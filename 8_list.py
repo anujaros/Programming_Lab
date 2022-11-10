220751702e2c22a5ff5d78124a564caf8712a03d
anujaros <71624832+anujaros@users.noreply.github.com>
2022-11-11 01:04:33 +0530

    Update 8_list.py

--- 8_list.py
+++ 8_list.py
@@ -1,5 +1,5 @@
 # list of positive integers
-a = input("enter the numbers")
+a = input("enter the numbers:")
 a = a.split()   #a = list(map(int,a))
 pl=list()
 a = [ int(x) for x in a]
@@ -14,15 +14,15 @@ b=[x*x for x in a]
 print ("The square of given numbers are:",b)
 
 #list of vowels
-v = input("enter a letter")
+v = input("enter a word:")
 vo = [ "a", "e", "i","o", "u", "A", "E","I","O","U"]
 a =[ x for x in v if x in vo]
-print(a)
+print("The vowels are:",a)
 
 #list of ordinal value
 ch = input("enter a letter")
 ch = [ ord(x) for x in ch]
-print(ch)
+print("The ordinal values are:",ch)
 
 
 

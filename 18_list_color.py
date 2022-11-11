6e0c840e11aaf024edd6bec5608ec2cd66e8d7cf
anujaros <71624832+anujaros@users.noreply.github.com>
2022-11-11 09:12:40 +0530

    Add files via upload

---
+++ 18_list_color.py
@@ -0,0 +1,4 @@
+colour_list1= ["white", "purple","blue","red","yellow","green"]
+colour_list2 =["orange", "white","blue","red","lavender"]
+new_lst = [c for c in colour_list1 if c not in colour_list2]
+print(new_lst)

0d5d5edc4778b2b2930f9df3dab79e3099ec54d1
anujaros <71624832+anujaros@users.noreply.github.com>
2022-12-09 23:21:49 +0530

    Update len_longestWord.py

--- len_longestWord.py
+++ len_longestWord.py
@@ -3,3 +3,12 @@ def len_of_longest(words):
 
 a=input("Enter A List of Words : ").split()
 print(len_of_longest(a))
+
+#def len_longest_word():
+#	wrds=input("Enter the words:").split()
+#	x=0
+#	for w in wrds:
+#		if len(w) > x:
+#			x= len(w)
+#	print(x)
+#len_longest_word()

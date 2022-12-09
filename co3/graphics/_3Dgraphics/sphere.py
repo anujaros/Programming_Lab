4adbafdeaa361757c48d62e7d001eb29e2f99b00
anujaros <71624832+anujaros@users.noreply.github.com>
2022-12-09 22:59:40 +0530

    Update sphere.py

--- co3/graphics/_3Dgraphics/sphere.py
+++ co3/graphics/_3Dgraphics/sphere.py
@@ -1,6 +1,16 @@
-from math import pi
-def surface_area(r):
-    return 4*pi*r**2
+import graphics as gp
+from graphics._3Dgraphics import *
+a= gp.circle_area(10) ; p = gp.circle_perimeter(10)
 
-def s_volume(r):
-    return 4/3*pi*r**3    
+print("Calling from graphics package:\nArea of circle:",a)
+print("Perimeter of circle:",p)
+
+print("Area of rectangle:",gp.r_area(5,10))
+print("Perimeter of rectangle:",gp.r_area(5,10),"\n")
+
+
+print("Calling from _3Dgraphics Subpackage:\nArea of SPhere:",surface_area(5))
+print("Volume of sphere:",s_volume(5))
+
+print("Area of cuboid:",total_s_area(4,8,10))
+print("Volume of cuboid:",cuboid_volume(4,8,10))

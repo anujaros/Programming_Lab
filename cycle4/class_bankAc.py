7277aaef9da34c02af3f6305014db395252bf508
Anuja Rose Baby <71624832+anujaros@users.noreply.github.com>
2022-12-23 10:55:21 +0530

    Update class_bankAc.py

--- cycle4/class_bankAc.py
+++ cycle4/class_bankAc.py
@@ -9,18 +9,20 @@ class Bank_Account:
     def deposit(self):
         dep = float(input("enter the amount to be deposited: "))
         self.balance += dep
-        self.balance()
+    
         
     def withdraw(self):
         wth = float(input("enter the amount to be withdrawn: "))
-        self.balance -= wth
-        self.balance()
-        
-    def balance(self):
-        if self.balance == 0:
-            print(" no balance ")
+        if wth > self.balance:
+            print("Not available Balance\n")
         else:
-            print(self.balance)
+            self.balance -= wth
+            
+        
+    def Balance(self):
+        print(self.balance)
+    
+        
 
 
 
@@ -31,14 +33,22 @@ ac_type = input("enter your account type: ")
 b1 = Bank_Account(name,ac_no,ac_type)
 
 while(True):
-    c = int(input(" 1 for deposit \n 2 for withdraw :"))
+    c = int(input("1.Deposit \n2.Withdraw \n3.Balance\n4.Exit\nEnter your choice:\n"))
 
     if c==1:
         b1.deposit()
+        
     elif c==2:
         b1.withdraw()
+        
+    elif c==3:
+        b1.Balance()
+        
+    elif c==4:
+        break
     else:
         print("wrong choice")
+
     
                     
                     
